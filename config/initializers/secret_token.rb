d6e49481e52cfea536c5ebebb64fe657e036a416
Dave Carter <dave@ubuntu.(none)>
2014-02-27 13:13:50 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knock4::Application.config.secret_key_base = '08b82e650c460ca1ab1caec1ccb5f78f93b430b4ef954a77eb7fc2f4fdf110dc9de135cc03b04cae8f08b6e8aa2dec8f3239f45de3563ceb4ebba4d31bdb32e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Knock4::Application.config.session_store :cookie_store, key: '_Knock4_session'
